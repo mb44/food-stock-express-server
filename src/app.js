3e8c50b8e6a7478db8b0a2ed33b80546562d44a2
Morten Beuchert <mortenbgood@gmail.com>
2018-04-02 19:07:05 +0200

    Add firebase

--- src/app.js
+++ src/app.js
@@ -9,13 +9,14 @@ app.use(bodyParser.json()) // allow the app to easily parse json requsts
 app.use(cors()) // allow any client to access this server (security risk)
 
 var admin = require('firebase-admin')
-
 var serviceAccount = require('../serviceAccountKey.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://foodwastereduction-6ca48.firebaseio.com'
 })
+
+var users = null
 
 // Firebase
 // Get a database reference to our posts
@@ -25,14 +26,15 @@ var ref = db.ref('users')
 // Attach an asynchronous callback to read the data at our posts reference
 ref.on('value', function (snapshot) {
   console.log(snapshot.val())
+  users = snapshot.val()
 }, function (errorObject) {
   console.log('The read failed: ' + errorObject.code)
 })
 
 // HTTP Endpoints
-app.get('/status', (req, res) => {
+app.get('/get-users', (req, res) => {
   res.send({
-    message: 'hello world!'
+    users: users
   })
 })
 
