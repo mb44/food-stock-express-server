a5fafda65ea2506f73b2aa05e0213375c97b3c12
Morten Beuchert <mortenbgood@gmail.com>
2018-04-02 15:00:57 +0200

    Add support for Firebase

--- src/app.js
+++ src/app.js
@@ -8,6 +8,28 @@ app.use(morgan('combined')) // for printing out logs
 app.use(bodyParser.json()) // allow the app to easily parse json requsts
 app.use(cors()) // allow any client to access this server (security risk)
 
+var admin = require('firebase-admin')
+
+var serviceAccount = require('../serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://foodwastereduction-6ca48.firebaseio.com'
+})
+
+// Firebase
+// Get a database reference to our posts
+var db = admin.database()
+var ref = db.ref('users')
+
+// Attach an asynchronous callback to read the data at our posts reference
+ref.on('value', function (snapshot) {
+  console.log(snapshot.val())
+}, function (errorObject) {
+  console.log('The read failed: ' + errorObject.code)
+})
+
+// HTTP Endpoints
 app.get('/status', (req, res) => {
   res.send({
     message: 'hello world!'
