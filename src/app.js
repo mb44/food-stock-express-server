aa90250e942933b6847ea4b6cab06cbb4fec0276
Morten Beuchert <mortenbeuchert@gmail.com>
2018-04-14 23:08:36 +0200

    Remove Morgan (for pretty printing)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const morgan = require('morgan')
 
 const app = express()
 app.use(function (req, res, next) {
