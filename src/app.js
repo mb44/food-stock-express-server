d97605b33e5747b4d442b9ecaa2eec6adc76f379
Morten Beuchert <mortenbeuchert@gmail.com>
2018-04-15 21:07:25 +0000

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-// const cors = require('cors')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
+
 app.use(morgan('combined')) // for printing out logs
 // allow the app to easily parse json requsts
 app.use(bodyParser.json())
-// app.use(cors()) // allow any client to access this server (security risk)
+app.use(cors()) // allow any client to access this server (security risk)
 
 var admin = require('firebase-admin')
 var serviceAccount = require('../serviceAccountKey.json')
